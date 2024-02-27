a0903bab181d6690b747b884ef98edb42edf8b36
Menglin-l <lynnlu.lml@gmail.com>
2024-02-26 21:51:18 -0800

    Day 4 JS

---
+++ DAY4-JAVASCRIPT-PART2/exercise.js
@@ -0,0 +1,12 @@
+function checkEvenOrOdd(num) {
+    if (num % 2 === 0) {
+        console.log("This is an even number.");
+    } else {
+        console.log("This is an odd number.");
+    }
+}
+let num1 = 7;
+let num2 = 10;
+
+checkEvenOrOdd(num1);
+checkEvenOrOdd(num2);
